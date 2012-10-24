679a541e4e9ea2b7fc4de8da7137d78c70d11019
Miriam <kihitara@gmail.com>
2012-10-24 15:20:16 +1300

    Fixed main content to work in 2.1

--- version.php
+++ version.php
@@ -17,5 +17,5 @@ $plugin->requires  = 2010112400; // Requires this Moodle version
 //To change the version requirement to 2.2, use 2011120100
 //Note that if you change the version requirement to lower than 2.2, it is at your own risk!
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '0.5 (Build: 2012070200)';
+$plugin->release = '0.6 (Build: 2012102400)';
 $plugin->component = 'theme_dragonslair'; // Full name of the plugin (used for diagnostics)
